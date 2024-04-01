797b71244836bceac74a6e98edfaee5eecd5aa94
WillianXG <wilian.mine789@gmail.com>
2024-04-01 19:46:09 -0300

    ProjetoFinal

--- projetoGrupo1.py
+++ projetoGrupo1.py
@@ -2,13 +2,35 @@ import os
 import csv
 from datetime import datetime
 
+def validar_resposta(resposta):
+    # Verifica se a resposta contém apenas números 1, 2 ou 3
+    if resposta not in ['1', '2', '3']:
+        return False
+    return True
+
+def validar_resposta1(genero):
+    # Verifica se a resposta contém apenas números de 1 a 10
+    if genero not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
+        return False
+    return True
+
+def validar_idade(idade):
+    try:
+        idade = int(idade)
+        if idade >= 0 and idade <= 120:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
 def main():
     # Define as perguntas da pesquisa
     perguntas = [
-        "Você pratica atividade física regularmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>>",
-        "Você consome pelo menos 5 porções de frutas e vegetais por dia? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>>",
-        "Você costuma dormir pelo menos 7 horas por noite? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>>",
-        "Você fuma atualmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>>"
+        "Você pratica atividade física regularmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+        "Você consome pelo menos 5 porções de frutas e vegetais por dia? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+        "Você costuma dormir pelo menos 7 horas por noite? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> ",
+        "Você fuma atualmente? \n 1 - Sim \n 2 - Não \n 3 - Não sei responder:\n>> "
     ]
 
     # Verifica se o arquivo já existe e está vazio
@@ -30,10 +52,19 @@ def main():
             idade = input("Informe a sua idade (00 para sair): ")
             if idade == '00':
                 break
-            genero = input("Qual o seu gênero?\n1- Masculino\n2- Feminino\n3- Não binário\n4- Agênero\n5- Gênero fluido\n6- Bigênero\n7- Transgênero\n8- Intersexo\n9- Outro\n10- Prefiro não dizer\n>>")
+            if not validar_idade(idade):
+                print("Idade inválida. Por favor, informe a sua idade novamente.")
+                continue
+
+            genero = input("Qual o seu gênero?\n1- Masculino\n2- Feminino\n3- Não binário\n4- Agênero\n5- Gênero fluido\n6- Bigênero\n7- Transgênero\n8- Intersexo\n9- Outro\n10- Prefiro não dizer\n>> ")
+            while not validar_resposta1(genero):
+                genero = input("Opção inválida. Por favor, selecione uma das opções de gênero listadas: ")
+
             respostas = []
             for pergunta in perguntas:
                 resposta = input(pergunta)
+                while not validar_resposta(resposta):
+                    resposta = input("Resposta inválida. Por favor, selecione uma das opções listadas: ")
                 respostas.append(resposta)
 
             # Obtém data e hora atual
@@ -46,4 +77,3 @@ def main():
     print("Dados salvos com sucesso no arquivo 'dados_pesquisa.csv'!")
 
 main()
-
